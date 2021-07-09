2c97d7ef0d3accf8d46fe3053aef92eee8becba3
David Polcari <dpolcari18@gmail.com>
2021-07-09 16:06:16 -0500

    began adding validations

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,9 @@
 class Family < ApplicationRecord
+    # relationships
     has_many :user_families
     has_many :users, through: :user_families
     has_many :recipes, dependent: :destroy
+
+    # validations
+    validates :name, presence: true, uniqueness: true
 end
--- app/models/ingredient_quantity.rb
+++ app/models/ingredient_quantity.rb
@@ -1,4 +1,8 @@
 class IngredientQuantity < ApplicationRecord
+    # relationships
     belongs_to :recipe
     belongs_to :ingredient
+
+    # validations
+    
 end
