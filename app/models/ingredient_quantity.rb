d4617a4d272114732e8549a0c096ba3492de2b3a
David Polcari <dpolcari18@gmail.com>
2021-07-09 16:13:26 -0500

    add validations for ingredient quantity

--- app/models/ingredient_quantity.rb
+++ app/models/ingredient_quantity.rb
@@ -4,5 +4,6 @@ class IngredientQuantity < ApplicationRecord
     belongs_to :ingredient
 
     # validations
-    
+    validates :ingredient_id, :recipe_id, :quantity, :unit, presence: true
+    validates :unit, inclusion: { in: %w(cup Oz flOz Tbsp tsp qt gal pt lb mL g kg l)}
 end
