8ddf4502b1bbd2470f47ca91c02da2b7d11bf6ba
David Polcari <dpolcari18@gmail.com>
2021-07-22 11:35:12 -0500

    added a third test to graphql query and refactored using let

--- spec/requests/graphql_spec.rb
+++ spec/requests/graphql_spec.rb
@@ -2,24 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "GraphQL", type: :request do
 
-    subject {
-        User.create(auth_id: 'authid')
-    }
-  describe "getUserInfo Query" do
-    it 'returns a hash with a user key given a proper id' do
-        variable = {id: subject.id}
-        query_string = "query getUserInfo($id: Int!) { user (id: $id) { id, families { id, name } } }"
-        response = FamilyKitchenBackendSchema.execute(query_string, variables: variable).to_h.symbolize_keys[:data]
+    let(:user) { User.create(auth_id: 'fakeid')}
+    let(:variable) { {id: user.id} }
+    let(:query_string) { "query getUserInfo($id: Int!) { user (id: $id) { id, families { id, name } } }" }
+    let(:response) { FamilyKitchenBackendSchema.execute(query_string, variables: variable).to_h }
 
-        expect(response).to include("user")
-    end
+    describe "getUserInfo Query" do
+        it 'returns a hash with a user key given a proper id' do
+            expect(response["data"]).to include("user")
+        end
 
-    it 'returns an error with message when not provided a valid id' do
-        variable = {id: 6}
-        query_string = "query getUserInfo($id: Int!) { user (id: $id) { id, families { id, name } } }"
-        response = FamilyKitchenBackendSchema.execute(query_string, variables: variable).to_h["errors"][0]
-        
-        expect(response).to include("message")
+        it 'returns an error with message when not provided a valid id' do
+            variable[:id] = 1
+            expect(response["errors"][0]).to include("message")
+        end
+
+        it 'returns a list of families the user is a part of' do
+            family = Family.create(name: 'New Family')
+            user_family = UserFamily.create(user_id: user.id, family_id: family.id, head: true)
+
+            expect(response['data']['user']['families']).to be_an(Array) 
+        end
     end
-  end
 end
